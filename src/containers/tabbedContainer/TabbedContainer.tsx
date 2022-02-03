85ea71f8a7ebbe4b37c4afeee5cd0526f219188d
artiom-hotinec <93658655+artiom-hotinec@users.noreply.github.com>
2022-02-03 11:53:09 +0200

    Update TabbedContainer.tsx

--- src/containers/tabbedContainer/TabbedContainer.tsx
+++ src/containers/tabbedContainer/TabbedContainer.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import TabbedComponent from "./tabbedComponent/TabbedComponent";
 import { TabbedContainerStyled } from './TabbedContainerStyled';
 
-
-
-
 export const TabbedContainer: React.FC  = () => {
 
     return (
