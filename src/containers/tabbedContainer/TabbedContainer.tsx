3b24520ecd2c4361ff6a0d25f2df2d55530f95ec
artiom-hotinec <93658655+artiom-hotinec@users.noreply.github.com>
2022-02-03 11:52:50 +0200

    Update TabbedContainer.tsx

--- src/containers/tabbedContainer/TabbedContainer.tsx
+++ src/containers/tabbedContainer/TabbedContainer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './TabbedContainer.css'
 import TabbedComponent from "./tabbedComponent/TabbedComponent";
 import { TabbedContainerStyled } from './TabbedContainerStyled';
 
